9246f0df8098df4d54190aba0fb385cd8972e4b5
ihusnainalii <41187664+ihusnainalii@users.noreply.github.com>
2018-07-27 18:52:18 +0500

    Add files via upload

--- customProgressBar/AppDelegate.swift
+++ customProgressBar/AppDelegate.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+extension UIApplication {
+    var statusBarView: UIView? {
+        return value(forKey: "statusBar") as? UIView
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
