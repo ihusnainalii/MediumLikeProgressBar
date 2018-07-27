011f207233c5c8fc5eb148d2c299a7a892f046b0
ihusnainalii <41187664+ihusnainalii@users.noreply.github.com>
2018-07-27 18:51:56 +0500

    Add files via upload

--- customProgressBar/ViewController.swift
+++ customProgressBar/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var btnChnageImage: UIButton!
+    
+    // MARK: - Variables
+    var progressViewManager: MediumProgressViewManager?
+    var isProfileImageSet = false
+    
+    // MARK: - view Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        progressViewManager = MediumProgressViewManager.sharedInstance
+        progressViewManager?.duration = 4
+        progressViewManager?.height = 2
+        progressViewManager?.showProgress(window: headerView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIApplication.shared.statusBarView?.backgroundColor = UIColor(red: 31/255, green: 162/255, blue: 200/255, alpha: 1.0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +39,27 @@ class ViewController: UIViewController {
     }
 
 
+    // MARK: - view Did Load
+    @IBAction func btnStartTapped(_ sender: UIButton) {
+        progressViewManager?.showProgress(window: headerView)
+    }
+    @IBAction func btnStopTapped(_ sender: UIButton) {
+        progressViewManager?.hide()
+    }
+    @IBAction func btnChnageImageTapped(_ sender: UIButton) {
+        isProfileImageSet = !isProfileImageSet
+        if isProfileImageSet {
+            progressViewManager?.hide()
+            progressViewManager?.showProgress(window: btnChnageImage)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.progressViewManager?.hide()
+                self.profileImage.image =  #imageLiteral(resourceName: "Contact-icon")
+            }
+        }else{
+            self.profileImage.image =  #imageLiteral(resourceName: "user")
+        }
+    }
+    
+    
 }
 
